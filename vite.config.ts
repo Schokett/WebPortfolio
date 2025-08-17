5830c11ff30ac9666a6e07e68c6edddcdee6a84b
Skrollan Elena Zimmermann <skrollanelenazimmermann@gmail.com>
2025-08-17 20:01:03 +0200

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,3 @@
 import { defineConfig } from 'vite';
 
-// Bei Project Page (USER.github.io/WebPortfolio/) BASE an Repo-Namen anpassen:
-export default defineConfig({
-  base: '/WebPortfolio/',   // exakt dein Repo-Name inkl. Groß/Klein
-});
+export default { base: '/WebPortfolio/' } 
